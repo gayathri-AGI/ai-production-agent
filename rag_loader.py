3033a41254ed4a9e4649631f9a0c3ca113b4ac69
V Gayathri Krishna <147518985+gayathri-AGI@users.noreply.github.com>
2026-02-04 14:16:00 +0530

    Update rag_loader.py

--- rag_loader.py
+++ rag_loader.py
@@ -1,6 +1,6 @@
 import os
 from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 def load_knowledge_base():
